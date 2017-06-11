413660ffa792e0202ff686486e0323e112b8132c
arun789 <adwiti1975@gmail.com>
2017-06-11 16:36:09 -0700

    new Fixed thread pool

---
+++ Multithreading/src/main/java/ExecutorDemo/NewFixedThreadPool.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package ExecutorDemo;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Adwiti
+ *
+ */
+public class NewFixedThreadPool {
+	public static void main(String[] args) {
+
+		/*
+		 * New Fixed Thread pool will execute maximum in the parameter
+		 * specified, rest will wait in the queue
+		 */
+		ExecutorService executes = Executors.newFixedThreadPool(3);
+		for (int id = 1; id <= 5; id++) {
+			executes.submit(new Task(id));
+		}
+		executes.shutdown();
+	}
+}
+
+class Task implements Runnable {
+
+	private int id;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+
+		for (int i = 0; i < 5; i++) {
+			try {
+				TimeUnit.MILLISECONDS.sleep(1000);
+				System.out.println("##### Task : " + id + " count : " + i + " #####");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * @param id
+	 */
+	public Task(int id) {
+		super();
+		this.id = id;
+	}
+
+}
