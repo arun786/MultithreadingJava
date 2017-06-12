e3bbfc5101246288c2b26b0eb1a77f137197be8f
arun789 <adwiti1975@gmail.com>
2017-06-11 17:36:42 -0700

    new single thread executor

--- Multithreading/src/main/java/ExecutorDemo/NewSingleThreadExecutor.java
+++ Multithreading/src/main/java/ExecutorDemo/NewSingleThreadExecutor.java
@@ -57,3 +57,60 @@ class TaskB implements Runnable {
 	}
 
 }
+
+/*o/p will be
+#########Task#######  1
+##### Task : 1 count : 0 #####
+##### Task : 1 count : 1 #####
+##### Task : 1 count : 2 #####
+##### Task : 1 count : 3 #####
+##### Task : 1 count : 4 #####
+#########Task#######  2
+##### Task : 2 count : 0 #####
+##### Task : 2 count : 1 #####
+##### Task : 2 count : 2 #####
+##### Task : 2 count : 3 #####
+##### Task : 2 count : 4 #####
+#########Task#######  3
+##### Task : 3 count : 0 #####
+##### Task : 3 count : 1 #####
+##### Task : 3 count : 2 #####
+##### Task : 3 count : 3 #####
+##### Task : 3 count : 4 #####
+#########Task#######  4
+##### Task : 4 count : 0 #####
+##### Task : 4 count : 1 #####
+##### Task : 4 count : 2 #####
+##### Task : 4 count : 3 #####
+##### Task : 4 count : 4 #####
+#########Task#######  5
+##### Task : 5 count : 0 #####
+##### Task : 5 count : 1 #####
+##### Task : 5 count : 2 #####
+##### Task : 5 count : 3 #####
+##### Task : 5 count : 4 #####
+#########Task#######  6
+##### Task : 6 count : 0 #####
+##### Task : 6 count : 1 #####
+##### Task : 6 count : 2 #####
+##### Task : 6 count : 3 #####
+##### Task : 6 count : 4 #####
+#########Task#######  7
+##### Task : 7 count : 0 #####
+##### Task : 7 count : 1 #####
+##### Task : 7 count : 2 #####
+##### Task : 7 count : 3 #####
+##### Task : 7 count : 4 #####
+#########Task#######  8
+##### Task : 8 count : 0 #####
+##### Task : 8 count : 1 #####
+##### Task : 8 count : 2 #####
+##### Task : 8 count : 3 #####
+##### Task : 8 count : 4 #####
+#########Task#######  9
+##### Task : 9 count : 0 #####
+##### Task : 9 count : 1 #####
+##### Task : 9 count : 2 #####
+##### Task : 9 count : 3 #####
+##### Task : 9 count : 4 #####
+*/
